f98a828c98994e1cbb4e972a1bb7f16fcb3669f7
Neil Ghosh <neil.ghosh@gmail.com>
2020-05-15 23:49:46 +0530

    Update main.go

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func echoHandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprint(w, "Hello Branch")
+	fmt.Fprint(w, "Hello Master")
 }
 
 func restHandler(w http.ResponseWriter, r *http.Request) {
